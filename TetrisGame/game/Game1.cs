f12ff8df7540839ae663e93f7cf656d1593f4b6c
AfterWind <afterwinddesu@gmail.com>
2015-01-26 14:48:10 +0200

    Mostly working board mechanics

--- TetrisGame/game/Game1.cs
+++ TetrisGame/game/Game1.cs
@@ -1,21 +1,19 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 
-namespace TetrisGame
-{
+namespace TetrisGame {
     /// <summary>
     /// This is the main type for your game
     /// </summary>
-    public class Game1 : Game
-    {
+    public class Game1 : Game {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        
 
-        public Game1()
-        {
+
+        public Game1() {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
@@ -26,8 +24,7 @@ namespace TetrisGame
         /// related content.  Calling base.Initialize will enumerate through any components
         /// and initialize them as well.
         /// </summary>
-        protected override void Initialize()
-        {
+        protected override void Initialize() {
             // TODO: Add your initialization logic here
             base.Initialize();
         }
@@ -36,8 +33,7 @@ namespace TetrisGame
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
         /// </summary>
-        protected override void LoadContent()
-        {
+        protected override void LoadContent() {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             GameObjects.Init(Content);
@@ -48,8 +44,7 @@ namespace TetrisGame
         /// UnloadContent will be called once per game and is the place to unload
         /// all content.
         /// </summary>
-        protected override void UnloadContent()
-        {
+        protected override void UnloadContent() {
             // TODO: Unload any non ContentManager content here
         }
 
@@ -58,11 +53,39 @@ namespace TetrisGame
         /// checking for collisions, gathering input, and playing audio.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime)
-        {
+        bool moved = false;
+        protected override void Update(GameTime gameTime) {
             // TODO: Add your update logic here
 
             GameObjects.board1.Update();
+            if (GameObjects.board1.hasLost)
+                this.Exit();
+            KeyboardState state = Keyboard.GetState();
+            if (state.GetPressedKeys().Length > 0) {
+                foreach (Keys k in state.GetPressedKeys()) {
+                    if (!moved) {
+                        if (k == Keys.Right) {
+                            GameObjects.board1.MoveShape(Block.size);
+                            moved = true;
+                        } else if (k == Keys.Left) {
+                            GameObjects.board1.MoveShape(-Block.size);
+                            moved = true;
+                        } else {
+                            moved = false;
+                        }
+                    }
+                    if (k == Keys.Escape) {
+                        this.Exit();
+                    } else if(k == Keys.P){
+
+                        GameObjects.board1.PrintMap();
+
+                    }
+                }
+            } else {
+                moved = false;
+            }
+
 
             base.Update(gameTime);
         }
@@ -71,16 +94,15 @@ namespace TetrisGame
         /// This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.Pink);
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
             spriteBatch.Begin(SpriteSortMode.FrontToBack, BlendState.AlphaBlend);
 
             //GameObjects.Update(spriteBatch);
             GameObjects.board1.Draw(spriteBatch);
-            
+
 
             spriteBatch.End();
 
--- TetrisGame/game/GameObjects.cs
+++ TetrisGame/game/GameObjects.cs
@@ -42,7 +42,11 @@ namespace TetrisGame {
         };
 
         public static Pattern[] patterns = new Pattern[] {
-            new Pattern(board1.PosX + board1.SizeX / 2, board1.PosY + 10, 1, 0, -1, 0, 0, 1, -1, -1, 1, -1)
+            new Pattern(1, 0, -1, 0, 0, 1, -1, -1, 1, -1),
+            new Pattern(0, 1, 0, 2, 0, 3),
+            new Pattern(1, 0, -1, 0, 0, 1, 0, -1),
+            new Pattern(1, 0, -1, 0, 0, -1),
+            new Pattern(1, 0, 0, -1, 1, -1),
         };
 
         public static Random random = new Random();
@@ -54,8 +58,7 @@ namespace TetrisGame {
             Random random = new Random();
             Pattern pattern = patterns[random.Next(0, patterns.Length)];
             Color color = GetRandomColor();
-            Console.Out.WriteLine("Got color: " + color.ToString());
-            return new Shape(GetRandomColor(), pattern.centerX, pattern.centerY, pattern.rest);
+            return new Shape(GameObjects.board1, GetRandomColor(), pattern.centerX, pattern.centerY, pattern.rest);
         }
     }
 
@@ -63,9 +66,14 @@ namespace TetrisGame {
         public int centerX, centerY;
         public int[] rest;
 
-        public Pattern(int centerX, int centerY, params int[] rest) {
-            this.centerX = centerX;
-            this.centerY = centerY;
+        public Pattern(params int[] rest) {
+            this.centerX = GameObjects.board1.PosX + GameObjects.board1.SizeX / 2;
+            int minY = 0;
+            for (int i = 1; i < rest.Length; i += 2) {
+                if (minY > rest[i])
+                    minY = rest[i];
+            }
+            this.centerY = minY * (-1) * Block.size + GameObjects.board1.PosY;
             this.rest = rest;
         }
     }
@@ -75,13 +83,24 @@ namespace TetrisGame {
         public static int size = 20;
 
         public Color color { private set; get; }
+        public Board board;
+
         public int X { private set; get; }
         public int Y { private set; get; }
 
-        public Block(Color color, int x, int y) {
+        public int GetRelativeX() {
+            return (X - board.PosX) / Block.size;
+        }
+
+        public int GetRelativeY() {
+            return (Y - board.PosY) / Block.size;
+        }
+
+        public Block(Board board, Color color, int x, int y) {
             this.X = x;
             this.Y = y;
             this.color = color;
+            this.board = board;
         }
 
         public void Move(int offX, int offY) {
@@ -97,14 +116,15 @@ namespace TetrisGame {
     public class Shape {
         public List<Block> blockList;
         public Block center;
+        public Board board;
 
-        public Shape(Color color, int centerX, int centerY, params int[] adjacent) {
-
+        public Shape(Board board, Color color, int centerX, int centerY, params int[] adjacent) {
+            this.board = board;
             blockList = new List<Block>();
-            center = new Block(color, centerX, centerY);
+            center = new Block(board, color, centerX, centerY);
             blockList.Add(center);
             for (int i = 0; i < adjacent.Length; i += 2) {
-                blockList.Add(new Block(color, centerX + adjacent[i] * Block.size, centerY + adjacent[i + 1] * Block.size));
+                blockList.Add(new Block(board, color, centerX + adjacent[i] * Block.size, centerY + adjacent[i + 1] * Block.size));
             }
         }
 
@@ -120,10 +140,67 @@ namespace TetrisGame {
             }
         }
 
-        public void Move(int offX, int offY) {
+        public bool MoveX(int offX) {
+            bool ok = true;
+
             foreach (Block block in blockList) {
-                block.Move(offX, offY);
+                int newX = block.X + offX;
+                if (newX < board.PosX)
+                    ok = false;
+                if (newX + Block.size > board.PosX + board.SizeX) {
+                    //block.Move(board.PosX + board.SizeX - newX - Block.size, 0);
+                    ok = false;
+                }
+                Block blockCheck = null;
+                try {
+                    blockCheck = board.blockArray[(newX - board.PosX) / Block.size, block.GetRelativeY()];
+                } catch(IndexOutOfRangeException ex) {
+
+                }
+                if (blockCheck != null) {
+                    block.Move(board.blockArray[(newX - board.PosX) / Block.size, block.GetRelativeY()].X - block.X, 0);
+                    ok = false;
+                }
             }
+            if (!ok)
+                return false;
+
+            foreach (Block block in blockList) {
+                block.Move(offX, 0);
+            }
+
+            return true;
+        }
+
+        public bool MoveY(int offY) {
+            bool ok = true;
+            foreach (Block block in blockList) {
+                int newY = block.Y + offY;
+                if (newY < board.PosY)
+                    ok = false;
+                if (newY + Block.size > board.PosY + board.SizeY) {
+                    block.Move(0, board.PosY + board.SizeY - newY - Block.size);
+                    ok = false;
+                }
+                Block blockCheck = null;
+                try {
+                    blockCheck = board.blockArray[block.GetRelativeX(), (newY - board.PosY) / Block.size];
+                } catch (IndexOutOfRangeException ex) {
+
+                }
+                if (blockCheck != null) {
+                    //block.Move(0, board.blockArray[block.GetRelativeX(), (newY - board.PosY) / Block.size].Y - block.Y);
+                    ok = false;
+                }
+            }
+            if (!ok)
+                return false;
+
+            foreach (Block block in blockList) {
+                block.Move(0, offY);
+            }
+
+            return true;
         }
 
         public void Draw(SpriteBatch batch) {
@@ -135,20 +212,22 @@ namespace TetrisGame {
 
     public class Board {
 
-        private List<Block> blocks;
+        public Block[,] blockArray;
         private Shape movingShape;
-        
+
+        public bool hasLost = false;
+
         public int SizeX {private set; get;}
         public int SizeY {private set; get;}
         public int PosX {private set; get; }
         public int PosY {private set; get;}
 
         public Board(int posX, int posY, int columns, int rows) {
+            blockArray = new Block[columns, rows];
             this.SizeX = columns * Block.size;
             this.SizeY = rows * Block.size;
             this.PosX = posX;
             this.PosY = posY;
-            blocks = new List<Block>();
         }
 
         public void AddShape(Shape shape) {
@@ -157,46 +236,54 @@ namespace TetrisGame {
 
         public void Update() {
             if (movingShape != null) {
-                if (!IsShapeObstructed(movingShape)) {
-                    movingShape.Move(0, 5);
+                if (movingShape.MoveY(3)) {
+                    
                 } else {
-                    foreach (Block block in movingShape.blockList)
-                        blocks.Add(block);
+                    foreach (Block block in movingShape.blockList) {
+                        blockArray[block.GetRelativeX(), block.GetRelativeY()] = block;
+                    }
                     movingShape = null;
                 }
             } else {
                 AddShape(GameObjects.GetRandomShape());
             }
+            for (int i = 0; i < blockArray.GetLength(0); i++ ) {
+                if (blockArray[i, 0] != null)
+                    hasLost = true;
+            }
+        }
+
+        public void PrintMap() {
+            Console.Out.WriteLine("Printing Map: ");
+            for (int j = 0; j < blockArray.Length / blockArray.GetLength(0); j++) {
+                for (int i = 0; i < blockArray.GetLength(0); i++ ) {
+                    try {
+                        if (blockArray[i, j] != null)
+                            Console.Out.Write("1 ");
+                        else
+                            Console.Out.Write("0 ");
+                    } catch {
+                        Console.Out.WriteLine("Error occured on reading: " + i + ", " + j);
+                    }
+                }
+                Console.Out.Write("\n");
+            }
         }
 
         public void Draw(SpriteBatch batch) {
-            foreach (Block block in blocks) {
-                block.Draw(batch);
+            foreach (Block block in blockArray) {
+                if (block != null) {
+                    block.Draw(batch);
+                }
             }
             if (movingShape != null)
                 movingShape.Draw(batch);
         }
 
-        public bool IsBlockObstructed(Block b) {
-            if (b.Y + Block.size + 1 > PosY + SizeY)
-                return true;
-            foreach (Block block in blocks) {
-                if (b.X >= block.X && b.X <= block.X + Block.size && b.Y + Block.size + 1 >= block.Y && b.Y + Block.size + 1 <= block.Y + Block.size) {
-                    return true;
-                }
+        public void MoveShape(int offsetX) {
+            if (movingShape != null) {
+                movingShape.MoveX(offsetX);
             }
-            return false;
         }
-
-        public bool IsShapeObstructed(Shape shape) {
-            IEnumerator<Block> bottom = shape.GetBottomBlocks();
-            while (bottom.MoveNext()) {
-                if (IsBlockObstructed(bottom.Current))
-                    return true;
-            }
-            return false;
-        }
-
-
     }
 }
